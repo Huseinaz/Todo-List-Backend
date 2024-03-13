dab4c36574d11920ec6b91be1804855e3418b910
Mohamad Ezzedien <husseinabouzeinab@gmail.com>
2024-03-13 22:33:30 +0200

    Connecting database

---
+++ back-end/connection.php
@@ -0,0 +1,14 @@
+<?php 
+
+$name="localhost";
+$user="root";
+$pass="";
+$db_name="todo_db";
+
+$connection=new mysqli($name,$user,$pass,$db_name);
+
+if ($connection -> connect_error) {
+    echo "Error connecting";
+} else {
+    echo "Connected";
+}
